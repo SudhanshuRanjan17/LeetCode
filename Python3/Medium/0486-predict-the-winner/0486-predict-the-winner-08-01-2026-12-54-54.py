4d7b3cfcdfb07641f63c65ee7bec5d0a721ad0cc
Sudhanshu Ranjan <139035070+sudhanshuranja@users.noreply.github.com>
2026-08-01 12:54:55 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.63%) - LeetHub

---
+++ Python3/Medium/0486-predict-the-winner/0486-predict-the-winner-08-01-2026-12-54-54.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = nums[:]
+        
+        for j in range(1, n):
+            for i in range(j - 1, -1, -1):
+                dp[i] = max(
+                    nums[i] - dp[i + 1],
+                    nums[j] - dp[i]
+                )
+        
+        return dp[0] >= 0
